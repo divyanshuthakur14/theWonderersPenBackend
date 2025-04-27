06f1c32f8a5bc4519ffbb1924be2339612f1f8bc
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-04-27 14:21:51 +0530

    first commit

---
+++ models/Contact.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    dateSubmitted: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+module.exports = Contact;
