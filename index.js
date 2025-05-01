24d767b803eb3b2317cea21f0a6f603afd390519
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-05-01 20:54:00 +0530

    commit

--- index.js
+++ index.js
@@ -102,19 +102,24 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
 
   jwt.verify(token, secret, {}, async (err, info) => {
     if (err) throw err;
+
     const { title, summary, content } = req.body;
+
+    // Check if the file is uploaded and accessible
+    const cover = req.file ? req.file.path : ""; // req.file.path will contain the Cloudinary URL
 
     const postDoc = await Post.create({
       title,
       summary,
       content,
-      cover: req.file.path,
+      cover, // This will store the Cloudinary image URL in the 'cover' field
       author: info.id,
     });
 
     res.json(postDoc);
   });
 });
+
 
 
 app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
